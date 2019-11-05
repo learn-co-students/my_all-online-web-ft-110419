dee86d810ca0bb2bfe600d319fed1ca7df32bf23
Nethelbert <gilblasse@gmail.com>
2019-11-05 14:01:21 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,9 +1,13 @@
 require 'pry'
 
 def my_all?(collection)
-  return_value = []
-  for num in collection do
-   return_value << yield(num)
-  end
-  !return_value.include?(false)
+  !collection.map {|num| yield(num)}.include?(false)
 end
+
+# def my_all (collection)
+#   return_value = []
+#   for num in collection do
+#     return_value << yield(num)
+#   end
+#   !return_value.include?(false)
+# end
