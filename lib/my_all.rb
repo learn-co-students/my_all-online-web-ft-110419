62ceed7c8a2fe1e770aa3ae0312bc0b400259dac
Patrick Grady <iminvisibl2u@gmail.com>
2019-11-06 21:04:20 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
 def my_all?(collection)
-
+  returns = []
+  i = 0 
+  while i < collection.length() 
+    returns.push(yield(collection[i]))
+    i += 1
+  end
+  returns.none?{|r| r == false}
 end
