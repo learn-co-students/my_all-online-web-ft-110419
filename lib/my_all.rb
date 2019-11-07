f57988c9c4b76290282ca811b281461582a73d25
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-07 18:39:31 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0
+  all = true 
+  while i < collection.length 
+    if yield(collection[i]) == false
+      all = false 
+      return all 
+    else
+      i += 1 
+    end
+  end 
+  all
 end
